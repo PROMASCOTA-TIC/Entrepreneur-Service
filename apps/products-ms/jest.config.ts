6e3cf9beee68a3c52c0fec0028b0493daa112e07
Jean Villacis <jean.villacis@epn.edu.ec>
2024-11-23 17:34:46 -0500

    feat: Crear y configurar microservicios con Nx y Nest

---
+++ apps/products-ms/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'products-ms',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/products-ms',
+};
