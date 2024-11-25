48fb695da3623b533e5ff2eac1c05aaa13bea688
Jean Villacis <jean.villacis@epn.edu.ec>
2024-11-24 21:20:22 -0500

    feat: Configurar variables de entorno y mejorar la inicialización del servidor en ms-products

--- apps/products-ms/src/main.ts
+++ apps/products-ms/src/main.ts
@@ -1,11 +1,9 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,13 +13,9 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     })
     );
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await app.listen(envs.port);
+  console.log(`Application is running on: ${envs.port}`);
+  
 }
 
 bootstrap();
